aaea729f9a589f4404062f902ecf900b757eea1d
wk <somnuk.wk@gmail.com>
2020-05-11 16:01:19 +0700

    By pass

---
+++ README.md
@@ -0,0 +1,9 @@
+## Reproduce
+
+```
+Unhandled exception. System.Net.Http.HttpRequestException: The SSL connection could not be established, see inner exception.
+ ---> System.Security.Authentication.AuthenticationException: The remote certificate is invalid according to the validation procedure.
+```
+
+
+- https://github.com/dotnet/corefx/pull/19908
---
+++ src/MyConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyConsole {
+    public class MyService {
+
+        private readonly IHttpClientFactory _factory;
+
+        public MyService(IHttpClientFactory factory) {
+            _factory = factory;
+        }
+
+        public async Task Get() {
+            var client = _factory.CreateClient("aaa");
+            var rs = await client.GetAsync("https://localhost:5001/weatherForecast");
+
+            Console.WriteLine(rs.StatusCode);
+        }
+    }
+
+    class Program {
+        static async Task Main(string[] args) {
+
+            // ServicePointManager.ServerCertificateValidationCallback =
+            // delegate (
+            //     object s,
+            //     X509Certificate certificate,
+            //     X509Chain chain,
+            //     SslPolicyErrors sslPolicyErrors
+            // ) {
+            //     return true;
+            // };
+
+            var collection = new ServiceCollection();
+
+            collection.AddHttpClient("aaa").ConfigurePrimaryHttpMessageHandler(() => {
+                var handler = new HttpClientHandler {
+                    ServerCertificateCustomValidationCallback = (httpRequestMessage, x509Certificate2, x509Chain, sslPolicyErrors) => {
+                        return true;
+                    },
+                };
+                // handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                handler.ServerCertificateCustomValidationCallback = delegate { return true; };
+                return handler;
+            });
+
+            collection.AddSingleton<MyService>();
+
+            var provider = collection.BuildServiceProvider();
+            var myService = provider.GetService<MyService>();
+
+            await myService.Get();
+        }
+    }
+}
